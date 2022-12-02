f12fc5cdaf3a71d8c4b700b42ffb655b531973c9
HdThinh6102 <20110404@student.hcmute.edu.vn>
2022-12-02 20:34:11 +0700

    add project

---
+++ Trigger_web_saleMotorcycles.sql
@@ -0,0 +1,106 @@
+Use web_Sale_motorcycle;
+
+-- 1. trigger tự tạo 1 row mới trong bảng specification khi tạo 1 row mới trong bảng motorcycles
+create trigger new_spec 
+after insert on motorcycles
+for each row
+insert into specification (motors_id) values ( NEW.motors_id);
+
+-- 2. trigger tự set attribute check_sold trong bảng a_motorcycle thành NULL nếu check_sale = stop sale trong bảng motorcycles, và trả về số lượng xe đó bằng 0 trogn bảng motorcycles
+delimiter // 
+create trigger after_update_check_sale
+after update on motorcycles
+for each row
+begin
+if old.check_sale != "stop sale" and NEW.check_sale = "stop sale" then
+update a_motorcycle set check_sold = NULL where check_sold ="not sole yet" and motors_id = new.motors_id;
+update motorcycles set quantity = 0 where motors_id = new.motors_id;
+end if;
+end;
+
+-- 3. trigger sau khi insert 1 row mới trong a_motorcycle thì trong motorcycles phần quantity sẽ tự + thêm 1
+create trigger after_insert_new_motor
+after insert on a_motorcycle
+for each row
+update motorcycles set quantity = quantity+1 where motors_id = new.motors_id;
+
+-- 4. trigger khi xóa 1 row trong a_motorcycle thì quantity tự -1 trong motorcycles
+create trigger after_delete_a_motor
+after delete on a_motorcycle
+for each row
+update motorcycles set quantity = quantity-1 where motors_id = old.motors_id;
+
+-- 5. trigger sau khi update bảng a_motorcycle cột check_sold=sold thì trong bảng motorcycles cột quantity-1
+-- 6. trigger sau khi update bảng a_motorcycle cột check_sold= not sold yet thì trong bảng motorcycles cột quantity+1
+delimiter //
+create trigger after_update_checksold_sold
+after update on a_motorcycle
+for each row
+begin
+if old.check_sold != new.check_sold and NEW.check_sold = "sold" then
+update motorcycles set quantity = quantity-1 where motors_id = new.motors_id;
+
+elseif old.check_sold != new.check_sold and NEW.check_sold = "not sold yet" then
+update motorcycles set quantity = quantity+1 where motors_id = new.motors_id;
+end if;
+end;
+
+-- 8. trigger sau khi update bảng payment status = notpaid hoặc paid, update bảng a_motocycle check_sold = ordered
+-- 9. trigger sau khi update bảng payment status = successful, update bảng a_motocycle check_sold = sold
+-- 10. trigger sau khi update bảng payment status = cancel, update bảng a_motocycle check_sold = not_sold_yet
+delimiter //
+create trigger update_status_payment
+after update on payment
+for each row
+begin
+if old.status != new.status and (new.status = "paid" or new.status = "not paid") then
+update a_motorcycle set check_sold = "ordered" where a_motorcycle.a_motor_id = new.a_motor_id;
+
+elseif old.status != new.status and new.status = "successful" then
+update payment set date_buy = current_timestamp where payment_id = new.payment_id;
+update a_motorcycle set check_sold = "sold" where a_motorcycle.a_motor_id = new.a_motor_id;
+
+elseif old.status != new.status and new.status = "cancel" then
+update a_motorcycle set check_sold = "not sold yet" where a_motorcycle.a_motor_id = new.a_motor_id;
+
+end if;
+end;
+ 
+-- 12. trigger sau khi thêm một cột trong user thì phần role_id của cột đó sẽ tự set thành role_id có role_name = user
+create trigger insert_user
+before insert on user
+for each row
+set new.role_id = (select role_id from role where role_name = "user");
+
+-- 13. Sau khi update a_motorcycle thì sẽ tự động set date_update bằng current_timestamp
+delimiter // 
+create trigger date_update_a_motorcycle
+after update on a_motorcycle
+for each row
+begin
+if old.date_update = new.date_update then
+update a_motorcycle set date_update = current_timestamp() where date_insert = old.date_insert;
+end if;
+end;
+
+-- 14. Sau khi update blog thì sữ tự động set date_update bằng curent_timestamp
+delimiter // 
+create trigger date_update_blog
+after update on blog
+for each row
+begin
+if old.date_update = new.date_update then
+update a_motorcycle set date_update = current_timestamp() where date_write = old.date_write;
+end if;
+end;
+
+
+
+
+
+
+ 
+
+
+
+
---
+++ create_table_web_saleMotorcycles.sql
@@ -0,0 +1,112 @@
+Create database web_Sale_motorcycle;
+Use web_Sale_motorcycle;
+
+CREATE TABLE MOTORCYCLES(
+	motors_id INT PRIMARY KEY,
+	motors_name varchar(50) not null,
+	version varchar(50) not null,
+	motor_type varchar(50),
+	branch varchar(50) not null,
+    style varchar(50),
+    quantity INT not null default 0,
+    motor_desc text,
+    check_sale ENUM('sale','stop sale') not null default 'sale',
+    constraint motors_id_name_ver unique(motors_id, motors_name, version)
+);
+
+CREATE TABLE A_MOTORCYCLE(
+	a_motor_id INT PRIMARY KEY,
+	motors_id INT not null,
+	color varchar(20),
+    prepaid_price FLOAT,
+    price FLOAT,
+    date_insert datetime default current_timestamp,
+    date_update datetime,
+	check_sold ENUM('sold','ordered','not sold yet') default 'not sold yet',
+    foreign key(motors_id) References MOTORCYCLES(motors_id)
+);
+
+CREATE TABLE SPECIFICATION(
+	motors_id INT PRIMARY KEY,
+	size varchar(20),
+	mass_kg	FLOAT,
+	saddle_height_cm FLOAT,
+	rpm INT,
+	engine_torque FLOAT,
+	horse_power varchar(30),
+	foreign key(motors_id) References MOTORCYCLES(motors_id)	
+);
+
+CREATE TABLE ROLE(
+	role_id INT PRIMARY KEY,
+	role_name varchar(50) unique,
+	role_decs text
+);
+
+CREATE TABLE USER(
+	user_id INT(5) zerofill PRIMARY KEY auto_increment ,
+	role_id INT,
+    email varchar(50),
+    user_pass varchar(50) not null,
+    user_name varchar(50) not null,
+    cccd varchar(20),
+    num_phone varchar(20),
+    address varchar(100),
+    check_delete ENUM('still','deleted') default 'still',
+	foreign key(role_id) References ROLE(role_id),
+    constraint email_check unique(email, check_delete)
+);
+
+create table customer(
+	 cus_id INT(5) zerofill primary key auto_increment,
+     cus_name varchar(50),
+     cccd varchar(20),
+     email varchar(50),
+     num_phome varchar(20),
+     address varchar(100)
+);
+
+CREATE TABLE BLOG(
+	blog_id INT Primary key auto_increment,
+	title text,
+	body text, 
+	date_write datetime default current_timestamp,
+	date_update datetime
+);
+
+CREATE TABLE PAYMENT(
+	payment_id INT(6) zerofill PRIMARY KEY auto_increment,
+	cus_id INT(5) zerofill,
+	a_motor_id INT,
+    name_cus varchar(50),
+    cccd varchar(20),
+    date_buy datetime,
+    date_prepaid datetime,
+    money_prepaid float,
+    price FLOAT,
+    payment_menthod varchar(30),
+    status ENUM('not paid', 'paid', 'successful', 'cancel'),
+	foreign key(a_motor_id) References A_MOTORCYCLE(a_motor_id),
+    foreign key(cus_id) References customer(cus_id)
+);
+
+CREATE TABLE COMMENT(
+	 cmt_id INT(5) zerofill PRIMARY KEY auto_increment,
+	 a_motor_id INT not null,
+	 user_id INT(5) zerofill not null,
+	 date_cmt datetime default current_timestamp,
+	 cmt_desc text,
+	 foreign key(a_motor_id) References a_motorcycle(a_motor_id),
+	 foreign key(user_id) References USER(user_id)
+);
+
+create table picturepro(
+	 picID INT Primary key,
+	 picName varchar(50),
+	 a_motor_id Int,
+	 foreign key(a_motor_id) References a_motorcycle(a_motor_id) 
+);
+
+
+
+
